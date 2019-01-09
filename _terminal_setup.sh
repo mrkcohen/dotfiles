e969a71e4e2b6ceabca5c7196f2700c790b42f69
Mike Cohen <mrkcohen@gmail.com>
2019-01-08 16:54:18 -0800

    small fix

--- _terminal_setup.sh
+++ _terminal_setup.sh
@@ -29,13 +29,13 @@ function switch_to_zsh {
 }
 
 function install_powerlevel9k {
-  if [ -d ${HOME}/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting/ ]; then
+  if [ -d ${HOME}/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting/ ] ; then
     echo "zsh sytnax highlighting already installed"
   else
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
   fi
 
-  if [ -d ${HOME}/.oh-my-zsh/cutom/themes/powerlevel9k/ ]; then
+  if [ -d ${HOME}/.oh-my-zsh/custom/themes/powerlevel9k/ ] ; then
     echo "Powerlevel9k theme already installed"
   else
     git clone https://github.com/bhilburn/powerlevel9k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel9k
